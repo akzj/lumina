96959a99f6b6e70c39f7a95f511186d2be2094c9
akzj <fu.niukey@gmail.com>
2026-05-01 08:28:19 +0800

    refactor(widget): unregister deprecated Go widgets (Button, Dialog, Toast, List, Pagination)
    Removed 5 deprecated widgets from widget.All() registration:
    Button, Dialog, Toast, List, Pagination
    
    These widgets have Lua lux replacements. The Go source files still exist
    but are no longer registered with the engine. Tests that reference
    lumina.Button etc. will need migration (next commit).

--- pkg/widget/register.go
+++ pkg/widget/register.go
@@ -3,18 +3,13 @@ package widget
 // All returns all built-in widgets.
 func All() []*Widget {
 	return []*Widget{
-		Button,
 		Checkbox,
 		Switch,
 		Radio,
 		Label,
 		Select,
-		Dialog,
 		Tooltip,
-		Toast,
 		Table,
-		List,
-		Pagination,
 		Menu,
 		Dropdown,
 		Spacer,
